86da75f266a70c3c27d14602c6dc7da7b61b2dd6
IgorDumi <igor.dumitrsakovic91@gmail.com>
2019-02-01 21:42:29 +0100

    created new class Student

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import student.Student;
+
+        
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        Student newStudent = new Student();
+        newStudent.info();
+    }
+    
+}
---
+++ src/student/Student.java
@@ -0,0 +1,64 @@
+
+package student;
+
+
+public class Student {
+    
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+    
+//    STUDENT CONSTRUCTORS START\
+    
+    public Student(){
+    this.firstName = "Petar";
+    this.lastName = "Petrovic";
+    this.yearOfBirth = 1950;
+    
+}
+    public Student( String firstName, String lastName, int yearOfBirth){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        
+    }
+    
+    public void info(){
+        System.out.println("First Name: " + this.firstName);
+        System.out.println("Last Name: " + this.firstName);
+        System.out.println("Year of Birth: " + this.yearOfBirth);
+    }
+
+//    STUDENT CONSTRUCTOR END
+    
+    
+//    SET AND GET START
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+    
+    //    SET AND GET END
+    
+
+}
