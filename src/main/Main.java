3d8e64557eade10e20ac0150a40ed62c35728962
Igor <igor.dumitraskovic91@gmail.com>
2019-02-02 12:33:56 +0100

    minor changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,37 +1,53 @@
 package main;
 
+import computer.Computer;
 import course.Course;
 import student.Student;
 
-        
 public class Main {
 
-
     public static void main(String[] args) {
-        
+
         Student defaultStudent = new Student();
         defaultStudent.info();
-        
-        Student igor = new Student();
-        igor.setFirstName("Igor");
-        igor.setLastName("Dumitraskovic");
-        igor.setYearOfBirth(1991);
-        
-        igor.getCourse().setCodeName("QA-3");
-        igor.getCourse().setName("QA");
-        igor.getCourse().setNumberOfClasses(33);
-        
-        igor.getComputer().setHardDrive(500);
-        igor.getComputer().setMemory(32);
-        igor.getComputer().setOperatingSystem("Windows");
-        igor.getComputer().setProcessTact(3.6);
-        
-        igor.info();
-        
-        
-                
-    }
-        
-    }
-    
 
+//        Student igor = new Student();
+//        igor.setFirstName("Igor");
+//        igor.setLastName("Dumitraskovic");
+//        igor.setYearOfBirth(1991);
+//
+//        igor.getCourse().setCodeName("QA-3");
+//        igor.getCourse().setName("QA");
+//        igor.getCourse().setNumberOfClasses(33);
+//
+//        igor.getComputer().setHardDrive(500);
+//        igor.getComputer().setMemory(32);
+//        igor.getComputer().setOperatingSystem("Windows");
+//        igor.getComputer().setProcessTact(3.6);
+//
+//        igor.info();
+//
+        Student nesa = new Student();
+        nesa.setFirstName("Nenad");
+        nesa.setLastName("Gladijator");
+        nesa.setYearOfBirth(1989);
+
+        nesa.getCourse().setCodeName("PHP123");
+        nesa.getCourse().setName("PHP");
+        nesa.getCourse().setNumberOfClasses(40);
+
+        nesa.getComputer().setHardDrive(1000);
+        nesa.getComputer().setMemory(16);
+        nesa.getComputer().setOperatingSystem("Linux");
+        nesa.getComputer().setProcessTact(2.6);
+
+//        Course phpCourse = new Course();
+//
+//        phpCourse.setCodeName("PHP123");
+//        phpCourse.setName("PHP");
+//        nesa.setCourse(phpCourse);
+//        nesa.getCourse().setNumberOfClasses(40);
+
+    }
+
+}
